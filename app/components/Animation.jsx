efc8315942ca89ba0334fac14e6e14d49513c4e2
Bisworanjan34 <bisworanjan34@gmail.com>
2024-11-02 00:13:23 +0530

    hello

---
+++ app/components/Animation.jsx
@@ -0,0 +1,48 @@
+"use client"
+import { faHeart, faThumbsUp, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useRef, useState } from 'react'
+import './Animation.css'
+
+const Animation = () => {
+    let [like,setLike]=useState(0)
+    let heartRef=useRef(null)
+    let clickfun = () => {
+      
+    heartRef.current.classList.toggle('active');
+    console.log("working");
+    
+    if(like ===0){
+        setLike(like+1)
+    }
+    else{
+        setLike(like-1)
+    }
+        
+    };
+  return (
+    <div className="" ref={heartRef}>
+       <h1 className='font-black text-blue-400 uppercase'>animation facebook-like</h1>
+         <div className="container border-2 w-72 h-96 flex justify-end items-center gap-1 relative shadow-xl shadow-cyan-400" >
+            <div className="facebook absolute top-2 font-bold left-2 border-b-2 bg-gradient-to-r from-sky-700 to-indigo-600">
+                <h1>Facebook</h1>
+            </div>
+            <div className="box text-md  mr-2 h-24 cursor-pointer flex w-10 flex-col items-center">
+                <FontAwesomeIcon icon={faUserCircle} className=' user mb-3 ' width={20}/>
+                <FontAwesomeIcon icon={faHeart} 
+                className='heart-click  mb-2' onClick={clickfun} width={20}/>
+                <p className=''>{like}view</p>
+            </div>
+             
+             <div className="heart absolute top-32 left-24 ">
+             <FontAwesomeIcon icon={faHeart} width={100} className='heart-show text-6xl'
+             />
+             </div>
+         </div>
+        
+         
+    </div>
+  )
+}
+
+export default Animation
