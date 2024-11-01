54de722cf3ea3dbceb5f13c847f9645b58ceebdf
Bisworanjan34 <bisworanjan34@gmail.com>
2024-11-02 00:17:53 +0530

    change

--- app/components/Animation.jsx
+++ app/components/Animation.jsx
@@ -22,10 +22,10 @@ const Animation = () => {
     };
   return (
     <div className="" ref={heartRef}>
-       <h1 className='font-black text-blue-400 uppercase'>animation facebook-like</h1>
+       <h1 className='font-black text-blue-400 uppercase'>click to show shre ram pic</h1>
          <div className="container border-2 w-72 h-96 flex justify-end items-center gap-1 relative shadow-xl shadow-cyan-400" >
-            <div className="facebook absolute top-2 font-bold left-2 border-b-2 bg-gradient-to-r from-sky-700 to-indigo-600">
-                <h1>Facebook</h1>
+            <div className="facebook absolute top-2 font-normal left-2 border-b-2 ">
+                <h1>{like?'Jayshree-ram':'Hello Soumyaranjan'}</h1>
             </div>
             <div className="box text-md  mr-2 h-24 cursor-pointer flex w-10 flex-col items-center">
                 <FontAwesomeIcon icon={faUserCircle} className=' user mb-3 ' width={20}/>
